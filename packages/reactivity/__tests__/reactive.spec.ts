9b5b86abe0e4e01eb7b1b03ddb38f4af9774f233
haoyang.ding <2698300641@qq.com>
2023-01-12 00:09:31 +0800

    feat(reactivity): add reactive method test case

--- packages/reactivity/__tests__/reactive.spec.ts
+++ packages/reactivity/__tests__/reactive.spec.ts
@@ -5,16 +5,17 @@ describe('reactivity/reactive', () => {
     const original = { foo: 1 };
     const observed = reactive(original);
     expect(observed).not.toBe(original);
-    // expect(isReactive(observed)).toBe(true);
-    // expect(isReactive(original)).toBe(false);
     // get
     expect(observed.foo).toBe(1);
+    // set
+    observed.foo++
+    expect(observed.foo).toBe(2)
+    expect(original.foo).toBe(2)
     //     // has
     expect("foo" in observed).toBe(true);
     //     // ownKeys
     expect(Object.keys(observed)).toEqual(["foo"]);
 
-    
-    console.log('pp', observed.__v_isReactive)
+    console.log('observed is reactive', observed.__v_isReactive)
   });
 })
