8ad6440d22a964027d5d5d93a6f1efb9a2396cf8
haoyang.ding <2698300641@qq.com>
2023-01-22 00:01:05 +0800

    feat: add hanChanged shared method

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,3 +4,5 @@ export const isObject = (val: unknown): val is Record<any, any> =>
 export const isArray = Array.isArray
 
 export const extend = Object.assign
+
+export const hasChanged = (value: unknown, oldValue: unknown): boolean => !Object.is(value, oldValue)
