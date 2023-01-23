03d10331ee3f9919ad06a58ff7ff84f2303c8b92
haoyang.ding <2698300641@qq.com>
2023-01-23 23:45:03 +0800

    feat(shared): add isFunction add NOOP method

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -6,3 +6,7 @@ export const isArray = Array.isArray
 export const extend = Object.assign
 
 export const hasChanged = (value: unknown, oldValue: unknown): boolean => !Object.is(value, oldValue)
+
+export const NOOP = () => {}
+
+export const isFunction = (value: unknown): value is Function => typeof value === 'function'
